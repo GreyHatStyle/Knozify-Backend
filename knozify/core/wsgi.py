67de2341ee48aed1271c60242a9fa11d8f6cd6a5
GreyHatStyle <manasbisht1142004@gmail.com>
2025-05-04 18:35:33 +0000

    Configured WSGI

--- knozify/core/wsgi.py
+++ knozify/core/wsgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+
+# application = get_wsgi_application()
+
 import os
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
-application = get_wsgi_application()
+def application(environ, start_response):
+
+    for key, value in environ.items():
+        if isinstance(value, str):
+            os.environ[key] = value
+
+    return get_wsgi_application()(environ, start_response)
